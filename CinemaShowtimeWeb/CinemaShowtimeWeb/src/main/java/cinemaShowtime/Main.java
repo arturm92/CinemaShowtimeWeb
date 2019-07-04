f285afc373945740d88ca6441e16d7539680762c
Artur <Artur@DESKTOP-C302AAA>
2019-07-05 00:07:52 +0200

    wygląd strony - header footer content

--- CinemaShowtimeWeb/CinemaShowtimeWeb/src/main/java/cinemaShowtime/Main.java
+++ CinemaShowtimeWeb/CinemaShowtimeWeb/src/main/java/cinemaShowtime/Main.java
@@ -8,34 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import util.Consts;
-import util.Utils;
-
 @WebServlet("/hi")
 
 public class Main extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	public Utils utils = new Utils();
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/plain; charset=UTF-8");
-		String json = ApiHelper.getDataFromApi(Consts.CITIES);
-		utils.listValues(json);
-		resp.getWriter().write("Response on cinema request: \n" + json);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//String param = req.getParameter("endpoint");
-		String json = ApiHelper.getDataFromApi(Consts.CITIES);
-		utils.listValues(json);
-		Object[] forwardParams = utils.getForwardParams();
-		req.setAttribute("list",forwardParams[0]);
-		req.getRequestDispatcher((String) forwardParams[1]).forward(req, resp);
-		
 	}
 
 }
