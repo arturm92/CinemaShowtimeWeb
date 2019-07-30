6848bfeeb2b3436be5543284758f73a60ae5f415
Artur <Artur@DESKTOP-C302AAA>
2019-07-30 21:06:00 +0200

    klasa HeaderBean dla menu

--- CinemaShowtimeWeb/CinemaShowtimeWeb/src/main/java/cinemaShowtime/beans/ControllerBean.java
+++ CinemaShowtimeWeb/CinemaShowtimeWeb/src/main/java/cinemaShowtime/beans/ControllerBean.java
@@ -1,23 +1,13 @@
 package cinemaShowtime.beans;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-
-import cinemaShowtime.ApiHelper;
-import model.json.movie.Movie;
 
 @ManagedBean(name = "controllerBean", eager = true)
 @SessionScoped
 public class ControllerBean {
 
 	private CityBean cityBean;
-	private List<Movie> headerMovies;
 	private MovieCatalogueBean movieCatalogueBean;
 
 	public ControllerBean() {
@@ -25,51 +15,13 @@ public class ControllerBean {
 
 		cityBean = new CityBean();
 		movieCatalogueBean = new MovieCatalogueBean();
-		headerMovies = ApiHelper.getNewestMovies().getMoviesWithPosterList();
 
 		long stopTime = System.currentTimeMillis();
-		System.out.println("Page start in " + ((stopTime - startTime) / 1000) + "second");
-	}
-
-	public void clickHeaderMovie() {
-		try {
-			ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-			String movieId = ec.getRequestParameterMap().get("movieId");
-			MovieDetailBean.getInstance().initMovieDetailBean(movieId);
-			updateHeaderMovieList(movieId);
-			ec.redirect("/CinemaShowtimeWeb/movies/movieDetail/index.xhtml");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private void updateHeaderMovieList(String movieId) {
-		List<Movie> newHeaderMovies = new ArrayList<Movie>();
-		List<Movie> tmp = new ArrayList<Movie>();
-		boolean copy = false;
-		for (Movie movie : headerMovies) {
-			if (copy) {
-				newHeaderMovies.add(movie);
-			} else {
-				if (movie.getId().compareTo(Long.valueOf(movieId)) == 0) {
-					newHeaderMovies.add(movie);
-					copy = true;
-				} else {
-					tmp.add(movie);
-				}
-			}
-		}
-		newHeaderMovies.addAll(tmp);
-		headerMovies = newHeaderMovies;
+		System.out.println("MainPage start in " + ((stopTime - startTime) / 1000) + "second");
 	}
 
 	public CityBean getCityBean() {
 		return cityBean;
-	}
-
-	public List<Movie> getHeaderMovies() {
-		return headerMovies;
 	}
 
 	public MovieDetailBean getMovieDetailBean() {
---
+++ CinemaShowtimeWeb/CinemaShowtimeWeb/src/main/java/cinemaShowtime/beans/HeaderBean.java
@@ -0,0 +1,65 @@
+package cinemaShowtime.beans;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import cinemaShowtime.ApiHelper;
+import model.json.movie.Movie;
+
+@ManagedBean(name = "headerBean", eager = true)
+@SessionScoped
+public class HeaderBean {
+	private List<Movie> headerMovies;
+
+	public HeaderBean() {
+		long startTime = System.currentTimeMillis();
+		
+		headerMovies = ApiHelper.getNewestMovies().getMoviesWithPosterList();
+		
+		long stopTime = System.currentTimeMillis();
+		System.out.println("FirstPage start in " + ((stopTime - startTime) / 1000) + "second");
+	}
+
+	public void clickHeaderMovie() {
+		try {
+			ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+			String movieId = ec.getRequestParameterMap().get("movieId");
+			MovieDetailBean.getInstance().initMovieDetailBean(movieId);
+			updateHeaderMovieList(movieId);
+			ec.redirect("/CinemaShowtimeWeb/movies/movieDetail/index.xhtml");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void updateHeaderMovieList(String movieId) {
+		List<Movie> newHeaderMovies = new ArrayList<Movie>();
+		List<Movie> tmp = new ArrayList<Movie>();
+		boolean copy = false;
+		for (Movie movie : headerMovies) {
+			if (copy) {
+				newHeaderMovies.add(movie);
+			} else {
+				if (movie.getId().compareTo(Long.valueOf(movieId)) == 0) {
+					newHeaderMovies.add(movie);
+					copy = true;
+				} else {
+					tmp.add(movie);
+				}
+			}
+		}
+		newHeaderMovies.addAll(tmp);
+		headerMovies = newHeaderMovies;
+	}
+
+	public List<Movie> getHeaderMovies() {
+		return headerMovies;
+	}
+}
